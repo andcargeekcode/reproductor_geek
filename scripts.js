ecfe2c630c0eda9450314c839ec819b168d814a1
Andres carmona <166671302+andcargeekcode@users.noreply.github.com>
2025-03-24 19:38:51 -0500

    Update scripts.js
    solucionando problema de ruta

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // Lista de canciones
 const songs = [
-    { title: "Canción 1", artist: "Artista 1", src: "Alcolirykoz _ Laberinto ELC - Los Legendarios (Prod. El Arkeólogo)(MP3_160K).mp3", cover: "andres.jpg" },
+    { title: "Canción 1", artist: "Artista 1", src: "./Alcolirykoz _ Laberinto ELC - Los Legendarios (Prod. El Arkeólogo)(MP3_160K).mp3", cover: "./andres.jpg" },
     { title: "Canción 2", artist: "Artista 2", src: "song2.mp3", cover: "cover2.jpg" }
 ];
 
